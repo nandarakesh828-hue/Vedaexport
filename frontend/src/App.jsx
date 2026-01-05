8d50ece816474f7d5e0949bf827d6de7ab9edc90
Nanda Rakesh <22jr1a43j5@gmail.com>
2026-01-05 06:00:11 +0530

    Test render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Cart from "./pages/Cart";
-
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
-    </BrowserRouter>
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      Frontend deployed successfully 🚀
+    </div>
   );
 }
+
+export default App;
